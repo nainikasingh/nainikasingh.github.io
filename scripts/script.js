c0ab983242ae42fa73b95f7e01cc270180e3b91e
Nainika Singh <141204621+nainikasingh@users.noreply.github.com>
2025-06-11 06:57:26 +0530

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+
+AOS.init();
+var typed = new Typed("#typed", {
+  strings: ["AI Developer", "Data Science", "AI ML", "Python Programmer", "Community Contributor"],
+  typeSpeed: 50,
+  backSpeed: 30,
+  loop: true
+});
+
+function toggleDarkMode() {
+  document.body.classList.toggle("dark");
+}
